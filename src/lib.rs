b6daa002d7462dc2fdbcd9244be6a72c9e13975e
Maga <steam.30mb1@gmail.com>
2023-07-02 19:27:02 +0400

    Parse64 up to new nekoton

--- src/lib.rs
+++ src/lib.rs
@@ -110,7 +110,7 @@ pub fn make_raw_contract_state(account: &str) -> Result<JsValue, JsValue> {
 #[wasm_bindgen(js_name = "parseMessageBase64")]
 pub fn parse_message_base64(message: &str) -> Result<JsValue, JsValue> {
     let msg = ton_block::Message::construct_from_base64(message).unwrap();
-    let nt_msg = nt::core::models::Message::from((msg.hash().unwrap(), msg));
+    let nt_msg = nt::core::models::Message::try_from((msg.hash().unwrap(), msg));
     serde_wasm_bindgen::to_value(&nt_msg).handle_error()
 }
 
